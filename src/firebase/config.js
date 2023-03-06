7980d385b2e057932519bd71b8c85946a4333cd9
Trocoli <ygoraraujoca@outlook.com>
2023-03-06 17:11:35 -0300

    deploy keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,10 +4,14 @@ import 'firebase/compat/firestore';
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey:FIREBASE_API_KEY ? FIREBASE_API_KEY : process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: FIREBASE_AUTH_DOMAIN ? FIREBASE_AUTH_DOMAIN : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId:FIREBASE_PROJECT_ID ? FIREBASE_PROJECT_ID : process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID 
+        apiKey: IzaSyCL-VaesZDIyzz_SobHQdT73Zqfi9moKRY,
+        authDomain: sc-chamados.firebaseapp.com,
+        projectId:gsc-chamados
     })
 }
 
 export default firebase 
+
+// apiKey:FIREBASE_API_KEY ? FIREBASE_API_KEY : process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+// authDomain: FIREBASE_AUTH_DOMAIN ? FIREBASE_AUTH_DOMAIN : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+// projectId:FIREBASE_PROJECT_ID ? FIREBASE_PROJECT_ID : process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID 
