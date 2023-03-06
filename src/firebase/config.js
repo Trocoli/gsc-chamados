c6a5fb0ca17aac51adf8a72c14b8480c10e0806a
Trocoli <ygoraraujoca@outlook.com>
2023-03-06 16:59:06 -0300

    change api keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,9 +4,9 @@ import 'firebase/compat/firestore';
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+        apiKey:FIREBASE_API_KEY ? FIREBASE_API_KEY : process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: FIREBASE_AUTH_DOMAIN ? FIREBASE_AUTH_DOMAIN : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId:FIREBASE_PROJECT_ID ? FIREBASE_PROJECT_ID : process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID 
     })
 }
 
