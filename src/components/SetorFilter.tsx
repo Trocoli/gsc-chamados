1e5a0c48b0c982240f62696e896fc1c95132243b
Trocoli <ygoraraujoca@outlook.com>
2023-03-07 15:53:47 -0300

    add setores in filter

--- src/components/SetorFilter.tsx
+++ src/components/SetorFilter.tsx
@@ -1,26 +1,39 @@
 interface SetorFilterProps {
-    placeholder?: string;
-    onChange: (value: any) => void;
-  }
-  
-  const SetorFilter = (props: SetorFilterProps) => {
-    return (
-      <div >  
-        <select
-          className="bg-gray-300 text-black text-center rounded-md"
-          name="setor"
-          id="setor"
-          onChange={e => props.onChange(e.target.value)}
-        >
-          <option value="">Setor</option>
-          <option value="GSC">GSC</option>
-          <option value="DEP" >DEP</option>
-          <option value="DCC" >DCC</option>
-          <option value="SUP" >SUP</option>
-        </select>
-      </div>
-    );
-  };
-  
-  export default SetorFilter;
-  
+  placeholder?: string;
+  onChange: (value: any) => void;
+}
+
+const SetorFilter = (props: SetorFilterProps) => {
+  return (
+    <div>
+      <select
+        className="bg-gray-300 text-black text-center rounded-md"
+        name="setor"
+        id="setor"
+        onChange={(e) => props.onChange(e.target.value)}
+      >
+        <option value="">Setor</option>
+        <option value="ARQ">ARQ</option>
+        <option value="ASP">ASP</option>
+        <option value="CPA">CPA</option>
+        <option value="CPL">CPL</option>
+        <option value="CON">CON</option>
+        <option value="DEP">DEP</option>
+        <option value="DCC">DCC</option>
+        <option value="DT">DT</option>
+        <option value="DOH">DOH</option>
+        <option value="DRF">DRF</option>
+        <option value="DA">DA</option>
+        <option value="DRA">DRA</option>
+        <option value="DRH">DRH</option>
+        <option value="GSC">GSC</option>
+        <option value="MAT">MAT</option>
+        <option value="PAT">PAT</option>
+        <option value="PJU">PJU</option>
+        <option value="SUP">SUP</option>
+      </select>
+    </div>
+  );
+};
+
+export default SetorFilter;
